5f3eeebbbb7db3f3aaf365f8109a9092bd03e3c1
Charishma <charishmachoppa@gmail.com>
2026-05-13 06:48:41 +0000

    Week 3 Task 1 submission

---
+++ raw_video.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+image_folder = "train/images"
+video_name = "raw_video.mp4"
+
+images = sorted(os.listdir(image_folder))
+
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(
+    video_name,
+    cv2.VideoWriter_fourcc(*'mp4v'),
+    20,
+    (width, height)
+)
+
+for image in images:
+    img_path = os.path.join(image_folder, image)
+    frame = cv2.imread(img_path)
+    video.write(frame)
+
+video.release()
+
+print("Raw video created.")
+
